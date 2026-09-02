8e3af791bd9cef6d4aa6eec60a3d9b3b9020e363
JPVillasanta <jhonpaulvillasanta937@gmail.com>
2026-09-02 16:00:34 +0800

    Implement CRUD operations for Task model in TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,14 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index()
     {
-        return response()->json([
-            'message' => 'Task list coming soon'
+        return Task::latest()->get();
+    }
+
+    public function store(Request $request)
+    {
+        return Task::create(
+            $request->validate([
+                'title' => 'required|string|max:255',
+            ])
+        );
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $task->update([
+            'is_done' => $request->boolean('is_done'),
         ]);
+
+        return $task;
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->noContent();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 Route::get('/tasks', [TaskController::class, 'index']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::patch('/tasks/{task}', [TaskController::class, 'update']);
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy']);
 Route::get('/', function () {
     return view('welcome');
 });
